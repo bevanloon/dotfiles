6165826729ef7f0ad0438cd58a0ac378bb6e9abf
Bevan <bevan.loon@digital.cabinet-office.gov.uk>
2021-05-01 16:49:28 +0100

    Recurse up through directory structure for venvup

--- config/.config/fish/functions/venvup.fish
+++ config/.config/fish/functions/venvup.fish
@@ -1,6 +1,18 @@
-function venvup
-  set -l base (basename $PWD)
+function try_activate -a dir
+  if test $dir = $HOME
+    return
+  end
+  if test $dir = /
+    return
+  end
+  set -l base (basename $dir)
   if test -e ~/.venv/$base/bin/activate.fish
     . ~/.venv/$base/bin/activate.fish
+  else
+    try_activate (dirname $dir)
   end
+end
+
+function venvup
+  try_activate $PWD
 end
